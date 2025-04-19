b4020deac785365edba84546902647dc84078b91
Ion Lipsiuc <ionlipsiuc@gmail.com>
2025-04-19 12:27:33 +0100

    fix: add missing header files

--- iteration.c
+++ iteration.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 // void set_initial_conditions(const int n, const int m, float* const matrix) {
 
 //   // Add two extra columns in order to simplify the 'wrapping around' problem
