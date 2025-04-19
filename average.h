a4b65cd909d7cf5c817d1586f175535277a8d077
Ion Lipsiuc <ionlipsiuc@gmail.com>
2025-04-19 14:06:59 +0100

    refactor: create average.h and iteration.h

---
+++ average.h
@@ -0,0 +1,7 @@
+#ifndef AVERAGE_H
+#define AVERAGE_H
+
+int average_rows(const int n, const int m, const float* const matrix,
+                 float* const averages);
+
+#endif // AVERAGE_H
---
+++ iteration.h
@@ -0,0 +1,6 @@
+#ifndef ITERATION_H
+#define ITERATION_H
+
+void iterations(const int iterations, const int n, const int m, float* matrix);
+
+#endif // ITERATION_H
