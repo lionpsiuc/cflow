dbcaeb22f6868904d267da383fe3cd2cd765e26b
Ion Lipsiuc <ionlipsiuc@gmail.com>
2025-04-20 22:29:03 +0100

    feat: header file for iteration-gpu.cu

---
+++ iteration-gpu.h
@@ -0,0 +1,14 @@
+#ifndef ITERATION_GPU_H
+#define ITERATION_GPU_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void init_gpu_wrapper(float* grid, int n, int m, float* timing);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
